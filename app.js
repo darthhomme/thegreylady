f78283e459beb220250cb653f7e3a90344e4675e
Jeong Kim <joshkim83@outlook.com>
2016-02-12 10:30:06 -0500

    heroku is not working for me

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var dotenv = require('dotenv');
 dotenv.load();
+//
+// mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/someweirdthing')
 
 app.use(express.static('./public'));
 
@@ -9,7 +11,7 @@ app.get('/', function(req, res, next){
   res.sendFile(__dirname + '/public/views/index.html');
 });
 
-var port = process.env.port || 8080;
+var port = 8080;
 app.listen(port, function(){
   console.log("Port " + port + " is up and running, Captain.");
 })
