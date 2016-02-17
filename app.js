43ba9fd2f466b68df670b817dc05402957fe74bd
Jeong Kim <joshkim83@outlook.com>
2016-02-16 20:38:07 -0500

    changed port address to env variable

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var favicon = require('serve-favicon');
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
-
 app.use(express.static('./public'));
+
 
 app.get('/', function(req, res, next){
   res.sendFile(__dirname + '/public/views/index.html');
 });
 
 var port = 8080;
-app.listen(port, function(){
+app.listen(process.env.PORT || port, function(){
   console.log("Port " + port + " is up and running, Captain.");
 })
